6edee4bea9884bef9ed1c5416ac0362c19406919
Jojojojojojojojojojojojojojojojojo <86447768+Jojojojojojojojojojojojojojojojojo@users.noreply.github.com>
2021-08-17 16:51:46 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,61 @@
-var canvas;
-var backgroundImage;
-var bgImg;
-var database;
-var form, player;
-var playerCount;
 
-function preload() {
-  backgroundImage = loadImage("./assets/background.png");
+ const Engine = Matter.Engine;
+ const World = Matter.World;
+ const Bodies = Matter.Bodies;
+ const Body = Matter.Body;
+
+ var paper,ground,side0,side1,side2;
+
+function preload(){
+	
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  database = firebase.database();
-  game = new Game();
-  game.start();
+	 createCanvas(800, 700);
+  
+	
+	 engine = Engine.create();
+	 world = engine.world;
 
+	 paper = new Paper(100, 600, 10);
+
+     ground = new Ground(400, 680, 800, 20);
+  
+	 side0 = new Dustbin(550, 620, 20, 100);
+     side1 = new Dustbin(635, 660, 150, 20);
+     side2 = new Dustbin(720, 620, 20, 100);
+
+
+     Engine.run(engine);
+  
 }
+
 
 function draw() {
-  background(backgroundImage);
+
+  rectMode(CENTER);
+  background(0);
+
+  Engine.update(engine);
+
+  paper.display();
+
+  ground.display();
+
+  side0.display();
+  side1.display();
+  side2.display();
+
+  drawSprites();
+ 
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+function keyPressed(){
+	if(keyCode === UP_ARROW)  {
+	   Matter.Body.applyForce(paper.body, paper.body.position, 
+		   {x:15, y: -15})
+	}
+
+  }
+
+
